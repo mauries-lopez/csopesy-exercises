34d7387adf17d462d78bd3a7ba9e0eb3d3a6bc88
Kathleen Mae Romblon <kathleen_romblon@dlsu.edu.ph>
2024-10-11 01:52:38 +0800

    FCFS thread within thread
    runProcess: thread for 1 CPU core to run the process
    executeProcess: thread for checking if CPU cores are available

--- MCO Base Code/ScheduleWorker.cpp
+++ MCO Base Code/ScheduleWorker.cpp
@@ -21,39 +21,26 @@ void ScheduleWorker::addProcess(std::shared_ptr<Process> process) {
     processList.push_back(process);
 }
 
+void ScheduleWorker::runProcess(int core) {
+    if (core == -1) { // available
+        core = 1; // In use
+        std::thread processIncrementLine(&Process::incrementLine, processList[0]);  //Start the Process
+        processIncrementLine.detach(); //Separate the thread of the process.
+        core = -1; // Available
+    }
+}
+
 void ScheduleWorker::executeProcess() {
 
     //First-Come First Serve Algorithm
     // Execute schedulerQueue by getting the first who entered the processList.
-    // Iterate to check for available CPU cores
-    //while (!processList.empty()) {
-        for (int i : cores) { // iterate through the 4 CPU cores to see if available
-            // Lock mutex
-            //std::lock_guard<std::mutex> lock(mtx);
-            //mtx.lock();
-            if (cores[i] == -1) { // Free, core can be used
-                cores[i] = 1; // set core to In Use
-                // Start the process
-                // call incrementLine from Process class here
-                std::thread processIncrementLine(&Process::incrementLine, processList.front());  //Start the Process
-                processIncrementLine.detach(); //Separate the thread of the process.
-
-                // Wait for the process to finish ; might be unnecessary since this is implemented in incrementLine() in Process
-                /*while (!processList[0]->isFinished()) {
-                    std::this_thread::sleep_for(std::chrono::milliseconds(100));
-                }*/
-
-                // Check if process in cores[index] is finished
-                // Include condition ??? but incrementLine should finish before it reaches this part of code
-                cores[i] = -1;
-
-                // Remove process at index 0 in processList
-                processList.erase(processList.begin());
-                //break;
-            }
-            //mtx.unlock();
+    while (!processList.empty()) {
+        // Iterate to check for available CPU cores (4)
+        for (int i : cores) {
+            std::thread core(runProcess, i);
+            core.detach();
         }
-    //}
+    }
 }
 
 void ScheduleWorker::displaySchedule() const {
--- MCO Base Code/ScheduleWorker.h
+++ MCO Base Code/ScheduleWorker.h
@@ -12,6 +12,7 @@ public:
     void addProcess(std::shared_ptr<Process> process);
     void executeProcess();
     void displaySchedule() const;
+    void runProcess(int core);
 
     std::vector<std::shared_ptr<Process>> schedulerQueue;
     std::vector<std::shared_ptr<Process>> processList;
